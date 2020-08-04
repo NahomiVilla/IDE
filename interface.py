4cb4c80b23221b1c3922ff8db0863598ca600a85
jlaica <jose.laica@protonmail.com>
2020-08-04 09:54:54 -0500

    Agregando funcion abrir archivo

--- interface.py
+++ interface.py
@@ -3,11 +3,18 @@ from tkinter import filedialog as FileDialog
 from io import open
 import sys
 
-def nuevo():
+def arch_nuevo():
     global ruta
     mensaje.set("Nuevo fichero")
     ruta = ""
     texto.delete(1.0, "end")
     root.title("GoQ - Editor")
 
-
+def abrir_arch():
+    global ruta
+    mensaje.set("Abrir fichero")
+    ruta = FileDialog.askopenfilename(
+    initialdir='.', 
+    filetypes=(("Ficheros python", "*.py"),),
+    title="Abrir un fichero python")
+    
