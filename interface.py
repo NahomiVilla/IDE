2304c1d59c03cb5650928b8ef5d3fd4b63d6dc86
jlaica <jose.laica@protonmail.com>
2020-08-04 09:47:13 -0500

    Agregando funcion nuevo archivo

--- interface.py
+++ interface.py
@@ -1,4 +1,13 @@
 from tkinter import *
 from tkinter import filedialog as FileDialog
 from io import open
+import sys
+
+def nuevo():
+    global ruta
+    mensaje.set("Nuevo fichero")
+    ruta = ""
+    texto.delete(1.0, "end")
+    root.title("GoQ - Editor")
+
 
